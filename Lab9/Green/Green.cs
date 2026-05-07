fb4e166ca0c9a107c139d2cae381c2c8bd58915b
Элен Сафарян <iravasu@Elens-MacBook-Air.local>
2026-05-07 18:25:36 +0300

    initial

---
+++ Lab9/Green/Green.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab9.Green
+{
+    public abstract class Green
+    {
+        private string _input;
+        public string Input => _input;
+
+        protected Green(string input)
+        {
+            _input = input;
+        }
+
+        public abstract void Review();
+
+        public virtual void ChangeText(string text)
+        {
+            _input = text;
+            Review();
+        }
+    }
+}
